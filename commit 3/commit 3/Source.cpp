3d664a06e53a12098337e53125251facaf33d098
BurikBur <onlyburik@gmail.com>
2020-10-08 18:36:02 +0300

    LastVersion
    Сделал передачу подстроки через аргумент

--- commit 3/commit 3/Source.cpp
+++ commit 3/commit 3/Source.cpp
@@ -4,37 +4,47 @@
 #include <string>
 using namespace std;
 
+
+void LogParser(string inputName, string outputName, string substr);
+
 int main()
+{
+    string inputName = "log.txt";
+    string outputName = "output.txt";
+    string substr = "Error";
+    LogParser(inputName, outputName, substr);
+    return 0;
+}
+
+void LogParser(string inputName, string outputName, string substr)
 {
     string line;
     string tfind;
     ifstream input;
     ofstream output;
-    string substr;
+    
     int k = 0;
     int index;
-    cout << "Enter the substr you want to find in the file: ";
-    cin >> substr;
-    output.open("output.txt");
-    input.open("log.txt");
-    cout << "----------------------------------------Reading file \"log.txt\"----------------------------------------" << endl << endl;
+    output.open(outputName);
+    input.open(inputName);
+    cout << "Reading file \"log.txt\"" << endl << endl;
     if (input.is_open())
     {
         while (!input.eof())
         {
             getline(input, line);
             k++;
-            cout << "----------------------------------------Reading " << k << " line----------------------------------------" << endl << endl;
+            cout << "Reading " << k << " line" << endl << endl;
             index = line.find(substr);
             if (index != -1) {
-                cout << "----------------------------------------Substring \"" << substr << "\" founded----------------------------------------" << endl << endl;
+                cout << "Substring \"" << substr << "\" founded" << endl << endl;
                 output << line << endl;
             }
-            else
-                cout << "----------------------------------------Substring \"" << substr << "\" not founded----------------------------------------" << endl << endl;
         }
-
     }
     input.close();
     output.close();
+    return;
 }
+
+
